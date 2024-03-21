ea0987135a5b8eadfbbf496ec5b9b9c7ad5ade0b
Aldrin Roger Perez Miranda <rogerperez1309@gmail.com>
2024-03-20 23:38:46 -0600

    add merge.ts

---
+++ merge.ts
@@ -0,0 +1,3 @@
+//Lo que paso haciendo el merge de la rama main y la rama feature/LAB04-01 
+//Definiendo primeramente que es merge: combinar los cambios de ambas ramas en este caso se combino los cambios de la rama main y la rama feature/LAB04-01
+//y se añadio el archivo Q-4.txt que no existia en la rama main, pero si estaba en la rama feature/LAB04-01
